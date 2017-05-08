b39df079217005df33d73b9c700544beffd10596
Hans Hagen <pragma@wxs.nl>
2017-05-08 18:02:00 +0200

    2017.05.08 18:02

--- tex/context/base/mkiv/util-sac.lua
+++ tex/context/base/mkiv/util-sac.lua
@@ -239,9 +239,9 @@ function streams.readfixed4(f)
     f[2] = j + 1
     local a, b, c, d = byte(f[1],i,j)
     if a >= 0x80 then
-        return (0x1000000 * a + 0x10000 * b + 0x100 * c + d - 0x100000000)/65536.0
+        return (0x100 * a + b - 0x10000) + (0x100 * c + d)/0x10000
     else
-        return (0x1000000 * a + 0x10000 * b + 0x100 * c + d)/65536.0
+        return (0x100 * a + b          ) + (0x100 * c + d)/0x10000
     end
 end
 
@@ -251,9 +251,9 @@ function streams.readfixed2(f)
     f[2] = j + 1
     local a, b = byte(f[1],i,j)
     if a >= 0x80 then
-        return (0x100 * a + b - 0x10000)/256.0
+        return (a - 0x100) + b/0x100
     else
-        return (0x100 * a + b)/256.0
+        return (a        ) + b/0x100
     end
 end
 
