ae3b88c79bd99c65c13cceb129f216459eee5c55
Hans Hagen <pragma@wxs.nl>
2017-11-24 15:57:00 +0100

    2017.11.24 15:57

--- tex/generic/context/luatex/luatex-fonts-merged.lua
+++ tex/generic/context/luatex/luatex-fonts-merged.lua
@@ -1,6 +1,6 @@
 -- merged file : c:/data/develop/context/sources/luatex-fonts-merged.lua
 -- parent file : c:/data/develop/context/sources/luatex-fonts.lua
--- merge date  : 11/21/17 19:33:07
+-- merge date  : 11/24/17 15:57:08
 
 do -- begin closure to overcome local limits and interference
 
