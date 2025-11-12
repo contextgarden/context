e9bd55ecd419385d9aa92e2034fb869048a82b30
hanshagen <j.hagen@xs4all.nl>
2025-11-12 23:38:03 +0100

    2025.11.12 23:08

--- tex/generic/context/luatex/luatex-fonts-merged.lua
+++ tex/generic/context/luatex/luatex-fonts-merged.lua
@@ -1,6 +1,6 @@
 -- merged file : c:/data/develop/context/sources/luatex-fonts-merged.lua
 -- parent file : c:/data/develop/context/sources/luatex-fonts.lua
--- merge date  : 2025-11-10 17:46
+-- merge date  : 2025-11-12 23:08
 
 do -- begin closure to overcome local limits and interference
 
